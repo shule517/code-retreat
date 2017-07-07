6aa6fccfe448fdb86d389006c3efccd78f0c4aeb
シュール <shule517@gmail.com>
2017-07-07 23:09:46 +0900

    initial commit

---
+++ lib/test.rb
@@ -0,0 +1,5 @@
+class Test
+  def hello
+    'world'
+  end
+end
---
+++ spec/test_spec.rb
@@ -0,0 +1,7 @@
+describe Test do
+  describe '#hello' do
+    it 'worldが取得できること' do
+      expect(Test.new.hello). to eq 'world'
+    end
+  end
+end
